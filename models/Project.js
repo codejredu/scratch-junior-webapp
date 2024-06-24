e6e659e6aee2a5bc682b045802ce57a9c90343ae
codejredu <146084539+codejredu@users.noreply.github.com>
2024-06-24 22:24:01 +0300

    Delete models directory

--- models/Project.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const ProjectSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-});
-module.exports = mongoose.model('Project', ProjectSchema);
