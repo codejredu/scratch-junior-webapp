1fdbc2b1021d63429f5d23ad89bc7a69ac4a3c7a
codejredu <146084539+codejredu@users.noreply.github.com>
2024-07-05 12:31:19 +0300

    Update microbit.js

--- microbit.js
+++ microbit.js
@@ -1,97 +1,33 @@
-class Scratch3MicrobitExtension {
-    constructor(runtime) {
+class MyExtension {
+    constructor (runtime) {
         this.runtime = runtime;
-        this.device = null;
-        this.ledCharacteristic = null;
+        this.runtime.registerExtension('MyExtension', this);
     }
 
-    getInfo() {
+    getInfo () {
         return {
-            id: 'microbitLED',
-            name: 'micro:bit LED',
+            id: 'myExtension',
+            name: 'My Extension',
             blocks: [
                 {
-                    opcode: 'connectMicrobit',
-                    blockType: 'command',
-                    text: 'Connect to micro:bit'
-                },
-                {
-                    opcode: 'setLED',
-                    blockType: 'command',
-                    text: 'Set LED at x: [X] y: [Y] to [STATE]',
+                    opcode: 'sayHello',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Say Hello to [NAME]',
                     arguments: {
-                        X: {
-                            type: 'number',
-                            defaultValue: 0
-                        },
-                        Y: {
-                            type: 'number',
-                            defaultValue: 0
-                        },
-                        STATE: {
-                            type: 'string',
-                            menu: 'ledStates',
-                            defaultValue: 'on'
+                        NAME: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'World'
                         }
                     }
                 }
-            ],
-            menus: {
-                ledStates: ['on', 'off']
-            }
+            ]
         };
     }
 
-    connectMicrobit() {
-        return navigator.bluetooth.requestDevice({
-            filters: [{ services: ['e95dd91d-251d-470a-a062-fa1922dfa9a8'] }]
-        })
-        .then(device => {
-            this.device = device;
-            return device.gatt.connect();
-        })
-        .then(server => server.getPrimaryService('e95dd91d-251d-470a-a062-fa1922dfa9a8'))
-        .then(service => service.getCharacteristic('e95d7b77-251d-470a-a062-fa1922dfa9a8'))
-        .then(characteristic => {
-            this.ledCharacteristic = characteristic;
-            return 'Connected to micro:bit';
-        })
-        .catch(error => {
-            console.error('Connection error:', error);
-            return 'Failed to connect';
-        });
-    }
-
-    setLED(args) {
-        if (!this.ledCharacteristic) {
-            return 'Not connected to micro:bit';
-        }
-
-        const x = Math.floor(args.X);
-        const y = Math.floor(args.Y);
-        const state = args.STATE === 'on';
-
-        if (x < 0 || x > 4 || y < 0 || y > 4) {
-            return 'Invalid LED position';
-        }
-
-        const ledMatrix = new Uint8Array(5);
-        const byteIndex = y;
-        const bitIndex = 4 - x;
-
-        if (state) {
-            ledMatrix[byteIndex] |= (1 << bitIndex);
-        } else {
-            ledMatrix[byteIndex] &= ~(1 << bitIndex);
-        }
-
-        return this.ledCharacteristic.writeValue(ledMatrix)
-            .then(() => `LED at (${x},${y}) set to ${state ? 'ON' : 'OFF'}`)
-            .catch(error => {
-                console.error('Error setting LED:', error);
-                return 'Failed to set LED';
-            });
+    sayHello (args) {
+        const name = args.NAME;
+        console.log(`Hello, ${name}!`);
     }
 }
 
-Scratch.extensions.register(new Scratch3MicrobitExtension());
+module.exports = MyExtension;
